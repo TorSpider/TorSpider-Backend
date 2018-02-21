49b7c32015c09cacb50743a76292d221150203ea
artagel <dan@syssec.biz>
2018-02-21 21:01:47 +0400

    Initial commit New deployment of the backend with API support.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # TorSpider-Backend
 The database backend with which the spiders share their discoveries.
+
+The database is a PostgreSQL database exposed via a Flask API.  
+
+
+## Usage
+The backend is responsible for the database and exposing the API.
+
+Update your app/config.py file with the PostgreSQL DB settings and ensure the database is created.
+
+Run the following command to initialize the database and create the tables:
+`python3 backend_manage.py initdb`
+
+Run the following command to seed the database:
+`python3 backend_manage.py seed`
+
+Let's get started:
+`ptyhon3 backend_manage.py run`
+
+You are now running your API, exposed on http://your_ip:1080
+
+## Set up Nginx
+Nginx is used to expose both the frontend (1081) and backend (1080) websites on one port (80) to regular users. 
+Ensure your have Nginx installed: `apt-get install nginx`
+
+Copy the provided nginx config file to /etc/nginx/sites-available:
+`cp nginx_conf/default /etc/nginx/sites-available`
+
+Restart Nginx:
+`service nginx restart`
+
---
+++ backend_manage.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+from os import environ
+import os
+import sys
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from app import app
+from app.models import *
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+manager = Manager(app)
+
+
+class DB(object):
+    def __init__(self, metadata):
+        self.metadata = metadata
+
+
+migrate = Migrate(app, DB(db.metadata))
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def run():
+    if not os.path.isdir(os.path.join(script_dir, 'logs')):
+        os.makedirs(os.path.join(script_dir, 'logs'))
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler = TimedRotatingFileHandler(os.path.join(script_dir, 'logs', 'TorSpider.log'), when='midnight', interval=1)
+    handler.setLevel(app.config['LOG_LEVEL'])
+    handler.setFormatter(formatter)
+    log = logging.getLogger('werkzeug')
+    log.setLevel(app.config['LOG_LEVEL'])
+    log.addHandler(handler)
+    app.logger.addHandler(handler)
+    port = int(environ.get('PORT', app.config['LISTEN_PORT']))
+    if app.config['USETLS']:
+        context = (os.path.join(script_dir, 'certs', 'server.crt'), os.path.join(script_dir, 'certs', 'server.key'))
+        app.run(host='0.0.0.0', port=port, threaded=True, ssl_context=context)
+    else:
+        app.run(host='0.0.0.0', port=port, threaded=True)
+
+
+@manager.command
+def initdb():
+    print("[+] Initializing database...")
+    print("[+] Creating tables...")
+    db.create_all(bind=None)
+    print('[+] Done!')
+
+
+@manager.command
+def seed():
+    # We'll populate the database with some default values. These
+    # pages are darknet indexes, so they should be a good starting
+    # point.
+    print('[+] Splinkle sprinkle!!!')
+    # TODO: Clean this up with relationships
+    seeds = [
+        # The Uncensored Hidden Wiki
+        # http://zqktlwi4fecvo6ri.onion/wiki/Main_Page
+        (Onions(domain='zqktlwi4fecvo6ri.onion'),
+         Urls(domain='zqktlwi4fecvo6ri.onion', url='http://zqktlwi4fecvo6ri.onion/wiki/Main_Page'),
+         Pages(domain='zqktlwi4fecvo6ri.onion', url='http://zqktlwi4fecvo6ri.onion/wiki/Main_Page')),
+        # OnionDir
+        # http://auutwvpt2zktxwng.onion/index.php
+        (Onions(domain='auutwvpt2zktxwng.onion'),
+         Urls(domain='auutwvpt2zktxwng.onion', url='http://auutwvpt2zktxwng.onion/index.php'),
+         Pages(domain='auutwvpt2zktxwng.onion', url='http://auutwvpt2zktxwng.onion/index.php')),
+        # Wiki links
+        # http://wikilink77h7lrbi.onion/
+        (Onions(domain='wikilink77h7lrbi.onion'),
+         Urls(domain='wikilink77h7lrbi.onion', url='http://wikilink77h7lrbi.onion/'),
+         Pages(domain='wikilink77h7lrbi.onion', url='http://wikilink77h7lrbi.onion/')),
+        # Deep Web Links
+        # http://wiki5kauuihowqi5.onion/
+        (Onions(domain='wiki5kauuihowqi5.onion'),
+         Urls(domain='wiki5kauuihowqi5.onion', url='http://wiki5kauuihowqi5.onion/'),
+         Pages(domain='wiki5kauuihowqi5.onion', url='http://wiki5kauuihowqi5.onion/')),
+        # OnionDir Deep Web Directory
+        # http://dirnxxdraygbifgc.onion/
+        (Onions(domain='dirnxxdraygbifgc.onion'),
+         Urls(domain='dirnxxdraygbifgc.onion', url='http://dirnxxdraygbifgc.onion/'),
+         Pages(domain='dirnxxdraygbifgc.onion', url='http://dirnxxdraygbifgc.onion/')),
+        # The Onion Crate
+        # http://7cbqhjnlkivmigxf.onion/
+        (Onions(domain='7cbqhjnlkivmigxf.onion'),
+         Urls(domain='7cbqhjnlkivmigxf.onion', url='http://7cbqhjnlkivmigxf.onion/'),
+         Pages(domain='7cbqhjnlkivmigxf.onion', url='http://7cbqhjnlkivmigxf.onion/')),
+        # Fresh Onions
+        # http://zlal32teyptf4tvi.onion/
+        (Onions(domain='zlal32teyptf4tvi.onion'),
+         Urls(domain='zlal32teyptf4tvi.onion', url='http://zlal32teyptf4tvi.onion/'),
+         Pages(domain='zlal32teyptf4tvi.onion', url='http://zlal32teyptf4tvi.onion/'))
+    ]
+
+    for seed in seeds:
+        # Sprinkle!!!!
+        db.session.add(seed[0])
+        db.session.commit()
+        db.session.add(seed[1])
+        db.session.commit()
+        db.session.add(seed[2])
+        db.session.commit()
+    print('[+] Done.')
+
+
+if __name__ == '__main__':
+    if sys.version_info[0] < 3:
+        raise Exception("Please use Python version 3 to run this script.")
+    manager.run()
