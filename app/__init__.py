2fda1e12fa2bb29bd7185d43681e50e80071ce56
artagel <dan@syssec.biz>
2018-02-23 20:16:15 +0400

    Add json serializer for sqlalchemy objects Add /api/next endpoint to better get the next url for the spiders Added logic so that we don't return urls with more than 1 try that the same node already scanned.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from os import environ
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask.json import JSONEncoder
+from contextlib import suppress
 
 app = Flask(__name__)
 
@@ -14,6 +16,16 @@ app.config.from_object(config_path)
 
 db = SQLAlchemy(app)
 
+class MyJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        # Optional: convert datetime objects to ISO format
+        with suppress(AttributeError):
+            return obj.isoformat()
+        return dict(obj)
+
+app.json_encoder = MyJSONEncoder
+
 from app import api, models
+from app.views import next_url
 
 
---
+++ app/views/next_url.py
@@ -0,0 +1,34 @@
+from flask import render_template, abort
+from flask_login import login_required
+from sqlalchemy import and_, or_
+from flask import jsonify
+from flask import request
+from app import app
+from app.models import Urls, Onions
+from datetime import date, timedelta
+import random
+import json
+
+
+@app.route("/api/next", methods=["GET"])
+# @login_required
+def next_url():
+    # TODO find a better serializer function
+    # We get the node name from the get param, otherwise leave it blank
+    try:
+        node_name = json.loads(request.args.get('q'))['node_name']
+    except:
+        node_name = None
+    week_ago = (date.today() - timedelta(days=7))
+    day_ago = (date.today() - timedelta(days=1))
+    candidates = Urls.query.join(Onions).filter(
+        or_(and_(Urls.fault == None, Urls.date < week_ago),
+            and_(Onions.online == True, Onions.tries != 0, Onions.last_node != node_name),
+            and_(Onions.online == False, Onions.scan_date < day_ago, Onions.last_node != node_name))).all()
+    if len(candidates) is 0:
+        return jsonify({'object': {}})
+    random_candidate = random.choice(candidates)
+    random_candidate_dict = dict(random_candidate)
+    random_candidate_dict['domain_info'] = dict(random_candidate_dict['domain_info'])
+    random_candidate_dict['domain_info']['urls'] = None
+    return jsonify({'objects': random_candidate_dict})
