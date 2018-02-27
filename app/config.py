204166ec30bef559737d5dce2916f926a4425319
artagel <dan@syssec.biz>
2018-02-27 20:55:47 +0400

    Change default listening address to localhost

--- app/config.py
+++ app/config.py
@@ -65,7 +65,7 @@ def make_config():
             'USETLS': False,
             'DEBUG': False,
             'LISTEN_PORT': 1080,
-            'LISTENING_ADDR': '0.0.0.0'
+            'LISTENING_ADDR': '127.0.0.1'
         }
         default_config['SQLAlchemy'] = {
             'SQLALCHEMY_ECHO': False,
