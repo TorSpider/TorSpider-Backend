13ae864dff9041845dec6de6bdafc9c771de3aa9
artagel <dan@syssec.biz>
2018-02-28 20:24:58 +0400

    Update nginx file with real address forwarding headers add proxyfix to flask.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask.json import JSONEncoder
 from contextlib import suppress
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 
@@ -10,6 +11,8 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 app.jinja_env.add_extension('jinja2.ext.do')
 app.threaded = True
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 config_path = environ.get("CONF_PATH", "app.config.ProductionConf")
 app.config.from_object(config_path)
