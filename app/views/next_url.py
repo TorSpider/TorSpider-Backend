fbf9ab1a422b46a78b5d8cdaa6c74f7bf29ba934
Unknown <eloquentmess@gmail.com>
2018-03-01 18:12:31 -0600

    From app import db

--- app/views/next_url.py
+++ app/views/next_url.py
@@ -2,7 +2,7 @@ from sqlalchemy import and_, or_
 from flask import jsonify
 from flask import request
 from flask import abort
-from app import app
+from app import app, db
 from app.helpers import check_api_auth
 from app.models import Urls, Onions
 from datetime import date, timedelta
